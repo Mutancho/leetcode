f191ac999f06e47a4ea33b996284b5c0e9657a60
DimitarP <dpolihronov1@gmail.com>
2023-12-08 08:42:27 +0200

    Problem 2942 js

---
+++ javascript/2942.js
@@ -0,0 +1,9 @@
+var findWordsContaining = function (words, x) {
+    let output = []
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) {
+            output.push(i)
+        }
+    }
+    return output
+};
