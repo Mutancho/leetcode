b68bb9e3c944843c733bcb23794935ef0ba6450b
DimitarP <dpolihronov1@gmail.com>
2023-12-11 13:43:05 +0200

    Problem 2331 js

---
+++ javascript/2331.js
@@ -0,0 +1,15 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function (root) {
+    if (root.val <= 1) return root.val
+    else if (root.val === 2) return evaluateTree(root.left) || evaluateTree(root.right)
+    return evaluateTree(root.left) && evaluateTree(root.right)
+};
